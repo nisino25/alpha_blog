b153b22e44c35d6ab53c8c4c9184e255199de9dd
nisino25 <nisino25@gmail.com>
2020-05-27 00:47:42 -0700

    Added some log in feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+    before_action :set_user, only:[:show,:edit,:update]
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -15,12 +15,12 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome #{@user.username}! you have successfully signed up"
             redirect_to articles_path
         else
@@ -30,7 +30,6 @@ class UsersController < ApplicationController
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your information was successfully uptdated"
             redirect_to @user
@@ -42,6 +41,10 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 
 
