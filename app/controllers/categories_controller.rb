7ee8e626e63b2dd96e92ce6c8e273a9def8dc7cc
nisino25 <nisino25@gmail.com>
2020-05-29 23:14:10 -0700

    Added new migration as article_category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,17 @@
 class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index, :show]
 
     def new
         @category = Category.new
     end
 
     def index
+        @categories = Category.paginate(page: params[:page], per_page:5)
+
     end
 
     def show
+        @category = Category.find(params[:id])
     end
 
     def create
@@ -24,6 +28,12 @@ class CategoriesController < ApplicationController
 
     def category_params
         params.require(:category).permit(:name)
+    end
+
+    def require_admin
+        if !(logged_in? && current_user.admin?)
+            flash[:alert] = "Only admin can do that"
+        end
     end
 
 end
